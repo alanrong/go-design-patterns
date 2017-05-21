56b1d673640949251e5543fa1a03cf4c8b94729d
yuntifree <yuntifree@qq.com>
2017-05-21 13:25:50 +0800

    add abstractfactory

---
+++ creational/abstractfactory/vehicle_factory.go
@@ -0,0 +1,62 @@
+package abstractfactory
+
+import (
+	"errors"
+	"fmt"
+)
+
+type VehicleFactory interface {
+	NewVehicle(v int) (Vehicle, error)
+}
+
+const (
+	CarFactoryType       = 1
+	MotorbikeFactoryType = 2
+)
+
+func BuildFactory(f int) (VehicleFactory, error) {
+	switch f {
+	case CarFactoryType:
+		return new(CarFactory), nil
+	case MotorbikeFactoryType:
+		return new(MotorbikeFactory), nil
+	default:
+		return nil, errors.New(fmt.Sprintf("Factory with id %d not recognized\n", f))
+	}
+}
+
+const (
+	LuxuryCarType = 1
+	FamilyCarType = 2
+)
+
+type CarFactory struct{}
+
+func (c *CarFactory) NewVehicle(v int) (Vehicle, error) {
+	switch v {
+	case LuxuryCarType:
+		return new(LuxuryCar), nil
+	case FamilyCarType:
+		return new(FamilyCar), nil
+	default:
+		return nil, errors.New(fmt.Sprintf("Vehicle of type %d not recognized\n", v))
+	}
+}
+
+type MotorbikeFactory struct{}
+
+const (
+	SportMotorbikeType  = 1
+	CruiseMotorbikeType = 2
+)
+
+func (m *MotorbikeFactory) NewVehicle(v int) (Vehicle, error) {
+	switch v {
+	case SportMotorbikeType:
+		return new(SportMotorbike), nil
+	case CruiseMotorbikeType:
+		return new(CruiseMotorbike), nil
+	default:
+		return nil, errors.New(fmt.Sprintf("vehicle of type %d not recognized\n", v))
+	}
+}
